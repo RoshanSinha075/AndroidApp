97bbdaf5e9a28e7d0de92422054149de6cb93517
RoshanSinha075 <64830187+RoshanSinha075@users.noreply.github.com>
2020-05-12 14:54:14 +0530

    Updated Log Comments
    Test

--- app/src/main/java/com/example/sampleapp/MainActivity.java
+++ app/src/main/java/com/example/sampleapp/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("Main Activity","Testing");
+        Log.d("Main Activity","Testing the file");
     }
 }
